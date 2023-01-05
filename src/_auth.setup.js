36dc5b73eca0483c9db29669c342941b0340b4e5
jahangir506 <zahangirkhan506@gmail.com>
2023-01-05 14:04:20 +0600

    ema-john-with-router-final

---
+++ src/_auth.setup.js
@@ -0,0 +1,44 @@
+/*
+-----------
+Auth Setup
+-----------
+1. Create firebase project
+2. enable sign in method
+3. install firebase
+4. get firebase config in your firebase.config.js file
+6. export app firebase.config.js
+
+*/
+
+/*
+--------------
+React UserContext
+--------------
+1. Create UserContext (Auth Context) : UserContext --> components name userContext provides AuthContext
+2. Create AuthContext
+3. set AuthContext.Provider
+4. Make sure you set the children
+5. export AuthContext to be used inside useContext hook
+6. get form data 
+7. getAuth in the UserContext
+*/
+
+/*
+-------------------
+FIREBASE HOSTING
+------------------
+// One time for each computer
+2. npm install -g firebase-tools
+3. firebase login
+
+// for each project one time
+3. firebase init
+Make sure: for public directory: you select: build
+single page application: y
+auto github : n
+
+
+// for every deploy
+4. npm run build
+5.firebase deploy
+*/
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAj-jg7i7xZ-R_312G6m7nzlZK2jtUzMzE",
+  authDomain: "ema-john-simple-d576a.firebaseapp.com",
+  projectId: "ema-john-simple-d576a",
+  storageBucket: "ema-john-simple-d576a.appspot.com",
+  messagingSenderId: "731438772470",
+  appId: "1:731438772470:web:2cbcfb176ad328f83c3cd7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
